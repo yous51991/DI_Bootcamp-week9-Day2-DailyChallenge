fae6545a47c4f3e5337cf8c11182700817ecec8e
Youssouf OUATTARA <121155997+yous51991@users.noreply.github.com>
2023-03-21 07:51:46 +0000

    Add files via upload

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StudentController extends AbstractController
+{
+
+    #[Route('/student/home', name: 'app_student')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/StudentController.php',
+        ]);
+    }
+    
+}
